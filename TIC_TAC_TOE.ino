5feaaad5444b40231c9b418d2a0aa41167a1cd79
MarcinhoLeo <43254790+MarcinhoLeo@users.noreply.github.com>
2019-11-25 01:07:24 +0300

    Update TIC_TAC_TOE.ino

--- TIC_TAC_TOE.ino
+++ TIC_TAC_TOE.ino
@@ -3,11 +3,16 @@
 #define pinX A0 //координата по Х
 #define pinY A1 //координата по У
 #define G 4 //кпопка при надавливании на стик
+#define DELAY 300
 
-int play =0;
 int place = 0;
 int turn = 0;
 int tn = 0;
+
+
+int valBut = 0; //задаю переменную, для счетчика 
+long previousMillis = 0; //задаю начальное значение для счетчика millis
+long TimePush = 5; //необходимое время нажатия на кнопку
 
 int x = 0;
 int y = 0;
@@ -30,6 +35,7 @@ void setup() {
   pinMode(pinX,INPUT);
   pinMode(pinY,INPUT);
   pinMode(G,INPUT);
+  digitalWrite(G, HIGH);
   place = 5; //начальнаое положение по середине
   turn = 1; //первый ход ( 1 для Х, 2 для О)
   lcd.InitLCD();
@@ -41,7 +47,6 @@ void setup() {
 }
 
 void loop() {
-  play = digitalRead(G);
   plc();//проверяю свободно ли место, если занято то перемещаю на след
   win();//пользуюсь если есть победитель
 
@@ -51,27 +56,27 @@ if ((analogRead(pinX) == 0) && (analogRead(pinY) >=0 )&& (analogRead(pinY) <= 70
      lcd.print((char *) " ",x,y);
     place= place-1;
     prv(); //перемещение на пред место по горизонтали
-    delay(300);
+    delay(DELAY);
    }else{
     lcd.print((char *) " ",x,y);
     place=9;
     prv();
-    delay(200);
+    delay(DELAY);
    }
 }
 
 //попадаю на правую четверть
 if ((analogRead(pinX) >= 680) && (analogRead(pinY) >=0 )&& (analogRead(pinY) <= 700)){
-   if(place != 9){
+   if((place != 9)){
     lcd.print((char *) " ",x,y);
     place= place+1;
     nxt();//пермещение на след место по горизонтали 
-    delay(300);
+    delay(DELAY);
    }else{
     lcd.print((char *) " ",x,y);
-    place=1;
+    place = 1;
     nxt();
-    delay(200);
+    delay(DELAY);
    }
 }
 
@@ -81,12 +86,12 @@ if ((analogRead(pinX) >= 80) && (analogRead(pinX) <= 700) && (analogRead(pinY) >
     lcd.print((char *) " ",x,y);
     place= place-3;
     prvv();//пермещение на пред место по вертикали
-    delay(300);
+    delay(DELAY);
    }else{
     lcd.print((char *) " ",x,y);
     place += 6;
     prvv();
-    delay(200);
+    delay(DELAY);
    }
 }
 
@@ -101,30 +106,44 @@ if ((analogRead(pinX) >= 80) && (analogRead(pinX) <= 700) && (analogRead(pinY) =
     lcd.print((char *) " ",x,y);
     place -=6;
     nxtv();
-    delay(200);
+    delay(DELAY);
    }
 }
 
 //нажатие на кнопку для выбора Х либо 0
-if(play == LOW){
+if(digitalRead(G) == LOW){
+  
   if(turn ==1){
     tn=1;
     lcd.print((char *) "X",x,y);
   }
+  
   if(turn ==2){
     tn=2;
     lcd.print((char *) "O",x,y);
   }
+
+  if(millis() - previousMillis  >=  1){// начинаем считать время нажатия
+    previousMillis = millis();
+    valBut++;// с каждой миллисекундой увеличиваем значение valBut
+  }           
+}else{ // если кнопку отпустили, то valBut становится равным 0
+     valBut = 0;
+     }
+if(valBut == TimePush){ // как толькозначение valLed становится равным TimtPush
   take();
   lcd.update();
-  delay(2000);
+  delay(300);
   turn++;
+       valBut = 10000; //защита от повторного действия при долгом нажатии
+ }
+  
   if(turn==3){
     turn=1;
   }
 }
-}
 
+//заполение ячеек
 void take(){
   if(place == 1){
     t1=tn;
@@ -277,7 +296,7 @@ void nxt (){
     place++;
   }
   if(place ==9 && t9 >=1){
-    place=1;
+    place = 1;
   }
 
 }
@@ -294,7 +313,7 @@ void nxtv (){
     place += 3;
   }
   if(place ==4 && t4 >=1){
-    place =+ 3;
+    place += 3;
   }
   if(place ==5 && t5 >=1){
     place += 3;
@@ -316,17 +335,17 @@ void nxtv (){
 
 //функция перемещ на пред место по вертикали 
 void prvv (){
-  if(place ==9 && t9 >=1){
+  if(place == 9 && t9 >=1){
     place -= 3;
   }
-  if(place ==8 && t8 >=1){
+  if(place == 8 && t8 >=1){
     place -= 3;
-  }
-  if(place ==7 && t7 >=1){
+  } 
+  if(place == 7 && t7 >=1){
     place -= 3;
   }
   if(place ==6 && t6 >=1){
-    place--;
+    place -= 3;
   }
   if(place ==5 && t5 >=1){
     place -= 3;
@@ -434,8 +453,6 @@ void prv (){
 //выявление победителя
 void win (){
   if(t1==1 && t2==1 && t3==1 || t4==1 && t5==1 && t6==1 || t7==1 && t8==1 && t9==1 || t1==1 && t5==1 && t9==1 || t3==1 && t5==1 && t7==1 || t1==1 && t4==1 && t7==1 || t2==1 && t5==1 && t8==1 || t3==1 && t6==1 && t9==1){
-    delay(1000);
-    lcd.update();
     delay(500);
     t1 =0;
     t2 =0;
@@ -447,11 +464,9 @@ void win (){
     t8 =0;
     t9 =0;
     lcd.drawBitmap(0,0,us_int,84,48);
-    place =1;
+    place = 5;
   }
   if(t1==2 && t2==2 && t3==2 || t4==2 && t5==2 && t6==2 || t7==2 && t8==2 && t9==2 || t1==2 && t5==2 && t9==2 || t3==2 && t5==2 && t7==2 || t1==2 && t4==2 && t7==2 || t2==2 && t5==2 && t8==2 || t3==2 && t6==2 && t9==2){
-    delay(1000);
-    lcd.update();
     delay(500);
     t1 =0;
     t2 =0;
@@ -463,11 +478,9 @@ void win (){
     t8 =0;
     t9 =0;
     lcd.drawBitmap(0,0,us_int,84,48);
-    place =1;
+    place = 5;
   }
   if(t1>=1 && t2>=1 && t3>=1 && t4>=1 && t5>=1 && t6>=1 && t7>=1 && t8>=1 && t9>=1){
-    delay(1000);
-    lcd.update();
     delay(500);
     t1 =0;
     t2 =0;
@@ -479,6 +492,6 @@ void win (){
     t8 =0;
     t9 =0;
     lcd.drawBitmap(0,0,us_int,84,48);
-    place =1;
+    place = 5;
   }
 }
