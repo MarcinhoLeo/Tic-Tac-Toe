dac618db9638f79803c969e4bb42bef70ec4abc5
MarcinhoLeo <43254790+MarcinhoLeo@users.noreply.github.com>
2019-11-25 01:36:22 +0300

    Update TIC_TAC_TOE.ino

--- TIC_TAC_TOE.ino
+++ TIC_TAC_TOE.ino
@@ -465,6 +465,7 @@ void win (){
     t9 =0;
     lcd.drawBitmap(0,0,us_int,84,48);
     place = 5;
+    turn = 1;
   }
   if(t1==2 && t2==2 && t3==2 || t4==2 && t5==2 && t6==2 || t7==2 && t8==2 && t9==2 || t1==2 && t5==2 && t9==2 || t3==2 && t5==2 && t7==2 || t1==2 && t4==2 && t7==2 || t2==2 && t5==2 && t8==2 || t3==2 && t6==2 && t9==2){
     delay(500);
@@ -479,6 +480,7 @@ void win (){
     t9 =0;
     lcd.drawBitmap(0,0,us_int,84,48);
     place = 5;
+    turn = 1;
   }
   if(t1>=1 && t2>=1 && t3>=1 && t4>=1 && t5>=1 && t6>=1 && t7>=1 && t8>=1 && t9>=1){
     delay(500);
@@ -493,5 +495,6 @@ void win (){
     t9 =0;
     lcd.drawBitmap(0,0,us_int,84,48);
     place = 5;
+    turn = 1;
   }
 }
